0969c88c31481540cc55799439008619a46b43fc
ClementineAccount <ClementineShamaney@gmail.com>
2023-03-21 17:05:58 +0800

    More NIH

--- src/Project/include/Project/ProjectApplication.hpp
+++ src/Project/include/Project/ProjectApplication.hpp
@@ -19,52 +19,6 @@ struct Vertex
     glm::vec4 Tangent;
 };
 
-struct MeshIndirectInfo
-{
-    uint32_t Count;
-    uint32_t InstanceCount;
-    uint32_t FirstIndex;
-    int32_t BaseVertex;
-    uint32_t BaseInstance;
-};
-
-struct MeshCreateInfo
-{
-    std::vector<Vertex> Vertices;
-    std::vector<uint32_t> Indices;
-    uint32_t TransformIndex;
-    uint32_t BaseColorTexture;
-    uint32_t NormalTexture;
-    size_t VertexOffset;
-    size_t IndexOffset;
-    uint32_t VertexBuffer;
-    uint32_t IndexBuffer;
-};
-
-struct Mesh
-{
-    uint32_t IndexCount = 0;
-    int32_t VertexOffset = 0;
-    uint32_t indexOffset = 0;
-    // NOT OpenGL handles, just indices
-    uint32_t TransformIndex = 0;
-    uint32_t BaseColorTexture = 0;
-    uint32_t NormalTexture = 0;
-};
-
-struct Model
-{
-    std::vector<Mesh> Meshes;
-    std::vector<uint32_t> Textures;
-    std::vector<glm::mat4> Transforms;
-    uint32_t InputLayout;
-    uint32_t VertexBuffer;
-    uint32_t IndexBuffer;
-    std::vector<uint32_t> Commands;
-    std::vector<uint32_t> ObjectData;
-    uint32_t TransformData;
-};
-
 class ProjectApplication final : public Application
 {
 protected:
@@ -76,8 +30,8 @@ protected:
     void Update() override;
 
 private:
-    Model _cubes;
     uint32_t _shaderProgram;
 
     bool MakeShader(std::string_view vertexShaderFilePath, std::string_view fragmentShaderFilePath);
+
 };
