23aa5315fd897a050a1efd84666aa038adb1a616
ClementineAccount <ClementineShamaney@gmail.com>
2023-05-08 17:24:01 +0800

    Refactor to setup a plain scene

---
+++ src/Tutorials/Main.cpp
@@ -0,0 +1,16 @@
+#include <ProjectApplication.hpp>
+
+int main(int argc, char* argv[])
+{
+    Tutorials::ProjectApplication application;
+    application.Run();
+    return 0;
+}
+
+
+//int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+//{
+//    ProjectApplication application;
+//    application.Run();
+//    return 0;
+//}
