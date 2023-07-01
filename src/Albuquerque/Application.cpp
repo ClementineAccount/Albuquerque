ed468bf2eda3365b6b0f496904fb24283dd2b8b1
ClementineAccount <ClementineShamaney@gmail.com>
2023-07-01 18:49:34 +0800

    Set OpenGL version to 4.6
    Fwog only has full support for 4.6. While my Linux virtual machine
    is limited to 4.5 for the moment, I should be setting it to 4.5
    manually when pulling.

--- src/Albuquerque/Application.cpp
+++ src/Albuquerque/Application.cpp
@@ -99,7 +99,7 @@ namespace Albuquerque
         glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
 
